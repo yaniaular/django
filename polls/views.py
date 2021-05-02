88c45ef6ca5342bb69a4ab66178b439cb10b4884
Yanina Aular <yaniaular@gmail.com>
2021-05-02 18:12:50 +0000

    First Assignment, week 2

--- polls/views.py
+++ polls/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. f3812346 is the polls index.")
