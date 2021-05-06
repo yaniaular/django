4919d43ee977b3e178f2aa5501159576a0171fc8
Yanina Aular <yaniaular@gmail.com>
2021-05-06 04:48:39 +0000

    Tutorial 03 django

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,11 +73,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+import os
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
